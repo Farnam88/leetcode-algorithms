19502ecb5ab0ee8178c6feb5051f9c3a02bd47c3
Farnam <masoud.jamshidian88@gmail.com>
2022-07-31 14:30:00 +0200

    remove unused projects

--- Algorithm.ConApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿//See https://aka.ms/new-console-template for more information
-
-
-using Algorithm.ConApp;
-
-Console.WriteLine("Starting");
-BenchmarkRunner.Run<Benchy>();
-Console.WriteLine("End");
-Console.ReadKey();
