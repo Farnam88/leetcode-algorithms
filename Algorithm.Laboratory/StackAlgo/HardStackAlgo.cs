65cc9530e98050cb72f3c0653e394b70ec063cc2
Farnam <masoud.jamshidian88@gmail.com>
2022-08-03 21:43:04 +0200

    84. Largest Rectangle in Histogram   - Problem function   - Test cases

---
+++ Algorithm.Laboratory/StackAlgo/HardStackAlgo.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Algorithm.Laboratory.StackAlgo;
+
+public class HardStackAlgo
+{
+    #region + LargestRectangleArea
+
+    /// <summary>
+    /// 84. Largest Rectangle in Histogram
+    /// https://leetcode.com/problems/largest-rectangle-in-histogram/
+    /// </summary>
+    /// <param name="heights"></param>
+    /// <returns></returns>
+    public int LargestRectangleArea(int[] heights)
+    {
+        return 0;
+    }
+
+    #endregion
+}
---
+++ Algorithm.Tests/StackAlgo/HardStackAlgoTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Algorithm.Tests.StackAlgo;
+
+public class HardStackAlgoTests
+{
+    private readonly HardStackAlgo _sut;
+
+    public HardStackAlgoTests()
+    {
+        _sut = new HardStackAlgo();
+    }
+
+    #region + LargestRectangleAreaTest
+
+    [Theory]
+    [InlineData(new int[] { 2, 4 }, 2)]
+    public void LargestRectangleAreaTest(int[] heights, int expected)
+    {
+        var result = _sut.LargestRectangleArea(heights);
+
+        Assert.Equal(expected, result);
+    }
+
+    #endregion
+}
