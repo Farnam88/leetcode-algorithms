b71ddd0c727e5b70c0ec763e9b286d2dcd7bca5a
Farnam <masoud.jamshidian88@gmail.com>
2022-08-09 10:51:19 +0200

    4. Median of Two Sorted Arrays

---
+++ Algorithm.Laboratory/BinarySearch/HardBinarySearch.cs
@@ -0,0 +1,47 @@
+﻿namespace Algorithm.Laboratory.BinarySearch;
+
+public class HardBinarySearch
+{
+    /// <summary>
+    /// 4. Median of Two Sorted Arrays
+    /// https://leetcode.com/problems/median-of-two-sorted-arrays/
+    /// </summary>
+    /// <param name="nums1"></param>
+    /// <param name="nums2"></param>
+    /// <returns></returns>
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        int[] shortArr = nums1.Length < nums2.Length ? nums1 : nums2;
+        int[] longArr = nums1.Length < nums2.Length ? nums2 : nums1;
+
+        int total = shortArr.Length + longArr.Length;
+        int half = total / 2;
+        int left = 0, right = shortArr.Length - 1;
+        
+        while (true)
+        {
+            int sMid = (int)Math.Floor((left + right) / (decimal)2);
+            int lMid = half - sMid - 2;
+
+            int shortMid = sMid >= 0 ? shortArr[sMid] : int.MinValue;
+            int shortRight = sMid + 1 < shortArr.Length ? shortArr[sMid + 1] : int.MaxValue;
+
+            int longMid = lMid >= 0 ? longArr[lMid] : int.MinValue;
+            int longRight = lMid + 1 < longArr.Length ? longArr[lMid + 1] : int.MaxValue;
+
+            if (shortMid <= longRight && longMid <= shortRight)
+            {
+                if (total % 2 != 0)
+                {
+                    return Math.Min(shortRight, longRight);
+                }
+
+                return (Math.Max(shortMid, longMid) + Math.Min(shortRight, longRight)) / (double)2;
+            }
+            else if (shortMid > longRight)
+                right = sMid - 1;
+            else
+                left = sMid + 1;
+        }
+    }
+}
---
+++ Algorithm.Tests/BinarySearch/HardBinarySearchTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Algorithm.Tests.BinarySearch;
+
+public class HardBinarySearchTests
+{
+    private readonly HardBinarySearch _sut;
+
+    public HardBinarySearchTests()
+    {
+        _sut = new HardBinarySearch();
+    }
+
+    #region MyRegion
+
+    [Theory]
+    [InlineData(new int[] { 1, 3 }, new int[] { 2 }, 2.00000)]
+    [InlineData(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.50000)]
+    [InlineData(new int[] { 4, 5, 6, 7, 8 }, new int[] { 0, 1, 3, 5 }, 5)]
+    public void FindMedianSortedArrays(int[] nums1, int[] nums2, double expected)
+    {
+        var result = _sut.FindMedianSortedArrays(nums1, nums2);
+
+        Assert.Equal(expected, result);
+    }
+
+    #endregion
+}
