4c2dbf3ef2ec30e8af108c74b887cb9b5321834a
Farnam <masoud.jamshidian88@gmail.com>
2022-08-11 15:26:25 +0200

    143. Reorder List

--- Algorithm.Laboratory/LinkedListAlgo/MediumLinkedListAlgo.cs
+++ Algorithm.Laboratory/LinkedListAlgo/MediumLinkedListAlgo.cs
@@ -2,5 +2,46 @@
 
 public class MediumLinkedListAlgo
 {
-    
+    #region + ReorderList
+
+    /// <summary>
+    /// 143. Reorder List
+    /// https://leetcode.com/problems/reorder-list/
+    /// </summary>
+    /// <param name="head"></param>
+    public void ReorderList(ListNode head)
+    {
+        ListNode slow = head, fast = head.next;
+
+        while (slow != null! && fast?.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode rightPortion = slow!.next, prev = null!;
+        slow!.next = null;
+
+        while (rightPortion != null)
+        {
+            var temp = rightPortion.next;
+            rightPortion.next = prev;
+            prev = rightPortion;
+            rightPortion = temp;
+        }
+
+        ListNode leftPortion = head;
+        rightPortion = prev;
+
+        while (rightPortion != null)
+        {
+            ListNode tempL = leftPortion.next, tempR = rightPortion.next;
+            leftPortion.next = rightPortion;
+            rightPortion.next = tempL;
+            rightPortion = tempR;
+            leftPortion = tempL;
+        }
+    }
+
+    #endregion
 }
--- Algorithm.Tests/LinkedListAlgo/MediumLinkedListAlgoTests.cs
+++ Algorithm.Tests/LinkedListAlgo/MediumLinkedListAlgoTests.cs
@@ -8,4 +8,32 @@ public class MediumLinkedListAlgoTests
     {
         _sut = new();
     }
+
+    #region + ReorderList
+
+    [Theory]
+    [MemberData(nameof(ReorderListData))]
+    public void ReorderListTest(ListNode head, ListNode expected)
+    {
+        _sut.ReorderList(head);
+
+        head.Should().BeEquivalentTo(expected);
+    }
+
+    public static IEnumerable<object[]> ReorderListData =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))),
+                new ListNode(1, new ListNode(4, new ListNode(2, new ListNode(3))))
+            },
+            new object[]
+            {
+                new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))),
+                new ListNode(1, new ListNode(5, new ListNode(2, new ListNode(4, new ListNode(3)))))
+            }
+        };
+
+    #endregion
 }
